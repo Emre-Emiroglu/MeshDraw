2dbaccf0d7e309145125cb75e8c0fefe84d3a568
Emre Emiroğlu <emirogluemre08@gmail.com>
2022-11-10 00:51:30 +0300

    Input system updates

--- Assets/Scripts/DrawingController/DrawingController.cs
+++ Assets/Scripts/DrawingController/DrawingController.cs
@@ -4,5 +4,56 @@ using UnityEngine;
 
 public class DrawingController : MonoBehaviour
 {
-  
+    #region Fields
+    [Header("Drawing Controller Fields")]
+    [SerializeField] private LineRenderer lr;
+    [SerializeField] private MeshCollider drawQuad;
+    [SerializeField] private Camera gameCamera;
+    [SerializeField] private Camera screenCamera;
+    private Pencil pencil;
+    private bool canDraw;
+    #endregion
+
+    #region Core
+    private void Awake()
+    {
+        pencil.OnInputEnded += onInputEnded;   
+    }
+    private void OnDestroy()
+    {
+        pencil.OnInputEnded -= onInputEnded;
+    }
+    #endregion
+
+    #region Receivers
+    private void onInputEnded()
+    {
+        lineToMesh();
+    }
+    #endregion
+
+    #region Executes
+    private void drawLine()
+    {
+    }
+    private void lineToMesh()
+    {
+    }
+    #endregion
+
+    #region Updates
+    public void Update()
+    {
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenCamera.farClipPlane);
+        canDraw = drawQuad.bounds.Contains(gameCamera.ScreenToWorldPoint(mousePos));
+        if (canDraw)
+        {
+            pencil.ExternalUpdate();
+            if (pencil.IsDragging)
+            {
+                drawLine();
+            }
+        }
+    }
+    #endregion
 }
--- Assets/Scripts/DrawingController/Pencil.cs
+++ Assets/Scripts/DrawingController/Pencil.cs
@@ -3,79 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-namespace DevShirme.PlayerModule
+public class Pencil
 {
-    [Serializable]
-    public class Pencil
+    #region Fields
+    public Action OnInputEnded;
+    private bool isDragging;
+    private Vector2 beganPos;
+    private Vector2 currentPos;
+    private Vector2 endPos;
+    private Vector2 deltaPos;
+    private Vector2 deltaNormal;
+    #endregion
+
+    #region Getters
+    public bool IsDragging => isDragging;
+    public Vector2 BeganPos => beganPos;
+    public Vector2 CurrentPos => currentPos;
+    public Vector2 EndPos => endPos;
+    public Vector2 DeltaPos => deltaPos;
+    public Vector2 DeltaNormal => deltaNormal;
+    #endregion
+
+    #region Core
+    private void clearInputs()
     {
-        #region Fields
-        [Header("Pencil Fields")]
-        [SerializeField] private MeshCollider drawQuad;
-        [SerializeField] private Camera gameCamera;
-        //----------------------------------------
-        private bool isDragging;
-        private Vector2 beganPos;
-        private Vector2 currentPos;
-        private Vector2 endPos;
-        private Vector2 deltaPos;
-        private Vector2 deltaNormal;
-        private bool canDraw;
-        #endregion
-
-        #region Core
-        public void ClearInputs()
-        {
-            isDragging = false;
-            canDraw = false;
-            beganPos = Vector2.zero;
-            currentPos = Vector2.zero;
-            endPos = Vector2.zero;
-            deltaPos = Vector2.zero;
-            deltaNormal = Vector2.zero;
-        }
-        #endregion
-
-        #region Input
-        private void onMouseDown()
-        {
-            beganPos = Input.mousePosition;
-        }
-        private void onDrag()
-        {
-            currentPos = Input.mousePosition;
-
-            deltaPos = currentPos - beganPos;
-            deltaNormal = deltaPos.normalized;
-
-            isDragging = deltaNormal.magnitude > .1f ? true : false;
-        }
-        private void onMouseUp()
-        {
-            endPos = Input.mousePosition;
-            ClearInputs();
-        }
-        public void ExternalUpdate()
-        {
-            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
-            canDraw = drawQuad.bounds.Contains(gameCamera.ScreenToWorldPoint(mousePos));
-            if (canDraw)
-            {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    onMouseDown();
-                }
-                if (Input.GetMouseButton(0))
-                {
-                    onDrag();
-                }
-                if (Input.GetMouseButtonUp(0))
-                {
-                    onMouseUp();
-                }
-            }
-            
-        }
-        #endregion
+        isDragging = false;
+        beganPos = Vector2.zero;
+        currentPos = Vector2.zero;
+        endPos = Vector2.zero;
+        deltaPos = Vector2.zero;
+        deltaNormal = Vector2.zero;
     }
-}
+    #endregion
 
+    #region Input
+    private void onMouseDown()
+    {
+        beganPos = Input.mousePosition;
+    }
+    private void onDrag()
+    {
+        currentPos = Input.mousePosition;
+
+        deltaPos = currentPos - beganPos;
+        deltaNormal = deltaPos.normalized;
+
+        isDragging = deltaNormal.magnitude > .1f ? true : false;
+    }
+    private void onMouseUp()
+    {
+        endPos = Input.mousePosition;
+
+        OnInputEnded?.Invoke();
+    }
+    public void ExternalUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            onMouseDown();
+        }
+        if (Input.GetMouseButton(0))
+        {
+            onDrag();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            onMouseUp();
+        }
+    }
+    #endregion
+}
